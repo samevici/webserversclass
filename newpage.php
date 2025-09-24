2ba1e31ddc11c5bd1f75956a6dfac4831430b0e6
samevici <victor.samoylenko@dawsoncollege.qc.ca>
2025-09-24 12:15:49 -0400

    friendlier and sanitized inputs

--- newpage.php
+++ newpage.php
@@ -6,6 +6,6 @@
 <body> 
     <h1> PHP dump! </h1>
     <p><?= $_SERVER['HTTP_USER_AGENT'];?></p>
-    <p>GET: <?= var_dump($_GET) ?></p>
-    <p>POST: <?= var_dump($_POST) ?></p>
+    <p><?= "Hello htmlspecialchars($_POST['fname'])  htmlspecialchars($_POST['lname'])"; ?></p>
+    <p><?= "You wanted to adopt a(n) htmlspecialchars($_POST['age']) cat ? ";?></p>
 </body>
