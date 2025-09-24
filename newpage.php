2517ac71827b997a271aba3672af6d27422a5866
samevici <victor.samoylenko@dawsoncollege.qc.ca>
2025-09-24 11:14:15 -0400

    get + put

--- newpage.php
+++ newpage.php
@@ -5,5 +5,7 @@
 
 <body> 
     <h1> PHP dump! </h1>
-    <p><?= $_SERVER['HTTP_USER_AGENT']; ?></p>
+    <p><?= $_SERVER['HTTP_USER_AGENT'];?></p>
+    <p>GET: <?= var_dump($_GET) ?></p>
+    <p>POST: <?= var_dump($_POST) ?></p>
 </body>
