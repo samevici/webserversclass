d4f7af7d1175ae42e8b9e9f3c80f0bb8c51374b5
samevici <victor.samoylenko@dawsoncollege.qc.ca>
2025-10-05 00:42:17 -0400

    making sure my site conforms to requirements (it is therfore no longer "lolrandom XD 2006-esque humour"-core

--- adoptapplication.php
+++ adoptapplication.php
@@ -7,5 +7,5 @@
     <h1> PHP dump! </h1>
     <p><?= $_SERVER['HTTP_USER_AGENT'];?></p>
     <p> Hello <?= htmlspecialchars($_POST['fname']) . " " . htmlspecialchars($_POST['lname']) ?></p>
-    <p>You wanted to adopt a <?=htmlspecialchars($_POST['age']) ?> cat</p>
+    <p>You wanted to adopt <?=htmlspecialchars($_POST['cat_name']) ?> cat</p>
 </body>
