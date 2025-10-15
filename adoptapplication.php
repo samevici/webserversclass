3081c6a63a62a58b4d84590a4903485ad00cea50
samevici <victor.samoylenko@dawsoncollege.qc.ca>
2025-10-14 23:02:31 -0400

    Using guid to keep track of form submissions

--- adoptapplication.php
+++ adoptapplication.php
@@ -17,6 +17,7 @@
 
                 $keys = [];
                 $resp = [];
+                $newid = uniqid();
 
                 foreach ($_POST as $x=>$y) {
                     $keys[] = "{$x}";
@@ -27,25 +28,29 @@
                     };
             
 
-                $sql = "INSERT INTO adoption_form (" .implode("," , $keys) . ") VALUES ('" . implode("','" , $resp) . "');";
+                $sql = "INSERT INTO adoption_form (id," . implode("," , $keys) . ") VALUES ( '{$newid}' " . ", '". implode("','" , $resp) . "');";
                 $result = mysqli_query($conn, $sql);
         ?>
 </head>
 
-<body> 
-    <h1> Application receipt </h1>
-        <?php    
-            $sql = "select * from adoption_form;";
-            $result = mysqli_query($conn, $sql);
-            $num = mysqli_num_rows($result);
-            $sql = "select * from adoption_form where id=$num;";
-            $result = mysqli_query($conn, $sql);
+    <body> 
+        <h1> Application receipt </h1>
+            <?php    
+        
+                $sql = "select * from adoption_form where id='{$newid}';";
+                $result = mysqli_query($conn, $sql);
+                foreach ($result as $row) {
 
-            echo "<p>Hello {$result['fname']} {$result['lname']}</p> 
-            <p> We received your application to adopt {$result['cat_name']}</p>
-            <p> We will contact you shortly about the next step in the adoption process</p>
-            ";
-        mysqli_close($conn);
-?>
-</body>
+                    $name = "{$row['fname']} " . "{$row['lname']}";
+                    $cat = "{$row['cat_name']}";
+                }
+
+                echo "<p>Hello {$name}</p> 
+                <p> We received your application to adopt {$cat}</p>
+                <p> We will contact you shortly about the next step in the adoption process</p>
+                ";
+
+                mysqli_close($conn);
+            ?>
+    </body>
 </html>
