ed760c375a2038f5c8eca0c219dc3b0dca732882
samevici <victor.samoylenko@dawsoncollege.qc.ca>
2025-11-19 12:09:40 -0500

    updated toggle

--- ledtoggle.php
+++ ledtoggle.php
@@ -2,32 +2,21 @@
 <html>
 <head>
 <title>led toggle page</title>
- <?php
 
-                $server = "localhost";
-                $username = "www-data";
-                $conn = mysqli_connect($server, $username, $password, $database);
-            
-                // Check for successful connection
-                if (!$conn) {
-                    die("Connection failed: {mysqli_connect_error()}");
-                }
-                
-        ?>
 <script>
     function myFunction() {
     <?php
-      $sql = "shell_exec('gpio toggle 15');";
-      $result = mysqli_query($conn, $sql);
+      $output = shell_exec('gpio toggle 15');
      ?>
     }
+    
 </script>
 
 </head>
 
 <body>
 
- <button onclick="myFunction()">Click to toggle</button> 
+<button type="button" onclick="myFunction()">Click to toggle</button> 
 
 </body>
 </html>
