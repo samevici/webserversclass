326368f79187654bb73dbc1359bf3520458f6ee5
samevici <victor.samoylenko@dawsoncollege.qc.ca>
2025-11-19 22:12:30 -0500

    some amendments

--- tempread.php
+++ tempread.php
@@ -1,27 +1,27 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>led toggle page</title>
+<title>reading page</title>
 
 <script>
     async function myFunction() {
         var xmlHttp = new XMLHttpRequest();
         xmlHttp.open( "GET", 'tempexec.php', true); 
         xmlHttp.send( null );
-        var result = JSON.parse(xmlHttp.responseText);
+        const rawResult = JSON.rawJSON(xmlHttp.responseText);
+        const result =JSON.parse(rawResult);
+
         document.getElementById("temp").innerHTML = result.temperature;
         document.getElementById("alt").innerHTML = result.altitude;  
         document.getElementById("press").innerHTML = result.pressure; 
-
     }
 
 </script>
-
 </head>
 
 <body>
-
 <button type="button" onclick="myFunction()">Click to read</button>
+<br>
 <b>Temperature:</b> <p id="temp"> 0.00</p>
 <b>Altitude:</b> <p id ="alt"> 0.00</p>
 <b>Pressure:</b> <p id = "press"> 0.00</p> 
